e284efc34d23ab04a57e762a9efa252217a01319
Anders Lundstedt <git@anderslundstedt.se>
2022-06-21 01:07:45 +0200

    log_info filter

--- _plugins/my_filters.rb
+++ _plugins/my_filters.rb
@@ -1,6 +1,12 @@
 # Modifed the example from https://talk.jekyllrb.com/t/how-to-properly-indicate-an-error-during-site-generation/447/2
 
 module MyFilters
+    def log_info(msg)
+        bad_file = @context.registers[:page]['path']
+        err_msg = "In #{bad_file}: #{msg}"
+        warn err_msg
+        return ""
+    end
     def log_warning(msg)
         bad_file = @context.registers[:page]['path']
         err_msg = "\033[33mIn #{bad_file}: #{msg}\033[0m"
