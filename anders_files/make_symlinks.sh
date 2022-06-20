f733a9fc24d954e8f74b7ed5d22c1fbf61e78790
Anders Lundstedt <git@anderslundstedt.se>
2022-06-21 01:27:21 +0200

    update my files

--- anders_files/make_symlinks.sh
+++ anders_files/make_symlinks.sh
@@ -2,11 +2,13 @@
 set -e
 cd $(dirname $0)
 cd ../.git/info
-rm exclude
-ln -s ../../anders_files/exclude .
+ln -sf ../../anders_files/exclude .
 cd ..
 cd ..
 ln -sf anders_files/envrc .envrc
 ln -sf anders_files/shell.nix .
 ln -sf anders_files/Makefile .
-ln -sf anders_files/vim .vim
+ln -sf anders_files/css.css.view .
+mkdir -p .vim
+cd .vim
+ln -sf ../anders_files/vimrc .
